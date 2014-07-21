62f31518c5ceab00edd381dc2a49c854e7fdde08
pdostal <git@pdostal.cz>
2014-07-21 13:43:28 +0200

    Configure server, port and bind address

--- app.rb
+++ app.rb
@@ -3,6 +3,9 @@ require 'sinatra'
 
 configure do
   enable :sessions
+  set :server, 'thin'
+  set port: 3000
+  set port: '0.0.0.0'
 end
 
 helpers do
@@ -23,14 +26,14 @@ get '/' do
   erb 'Can you handle a <a href="/secure/place">secret</a>?'
 end
 
-get '/login/form' do 
+get '/login/form' do
   erb :login_form
 end
 
 post '/login/attempt' do
   session[:identity] = params['username']
   where_user_came_from = session[:previous_url] || '/'
-  redirect to where_user_came_from 
+  redirect to where_user_came_from
 end
 
 get '/logout' do
